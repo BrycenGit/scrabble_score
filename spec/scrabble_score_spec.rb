3879b7d72c1b1f4f69c4c5e1d83d072d240e405a
Brycen Bartolome <brycenbartolome@gmail.com>
2020-09-22 09:50:47 -0700

    pass test,returns a scrabble score for a letter
    co_authored_by: Vanessa Stewart <vstewart1213@gmail.com>

--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -1,8 +1,9 @@
 require('rspec')
 require('scrabble_score')
 
-describe('#scrabble_score') do
+describe('Word#scrabble_score') do
   it("returns a scrabble score for a letter") do
-    expect("a".scrabble()).to(eq(1))
+    word = Word.new("a")
+    expect(word.scrabble_score()).to(eq(1))
   end
 end

1f99bf76fd54f8bac192a40f4f450eba0d86a576
Brycen Bartolome <brycenbartolome@gmail.com>
2020-09-22 09:56:09 -0700

    fail test,returns a scrabble score for the word 'hello'
    co_authored_by: Vanessa Stewart <vstewart1213@gmail.com>

--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -6,4 +6,9 @@ describe('Word#scrabble_score') do
     word = Word.new("a")
     expect(word.scrabble_score()).to(eq(1))
   end
+
+  it("returns a scrabble score for the word 'hello' ") do
+    new_word = Word.new("hello")
+    expect(new_word.scrabble_score()).to(eq(0))
+  end
 end

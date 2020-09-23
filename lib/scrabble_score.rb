c5a68878e7fc0c2094aacfdc07b78b2cb7c93cd9
Brycen Bartolome <brycenbartolome@gmail.com>
2020-09-23 16:21:10 -0700

    refactor code add sentence class, make more modular
    Co-Authored-By: Alex Goodwin <hagoodwi@gmail.com>
    co-authored-by: brycen bartolome <brycenbartolome@gmail.com>

--- lib/scrabble_score.rb
+++ lib/scrabble_score.rb
@@ -1,13 +1,29 @@
 class Word
-  def initialize (string)
-    @word = string.downcase()
-    @scores = { "a" => 1, "b" => 3, "c" => 3, "d" => 2, "e" => 1, "f" => 4, "g" => 2, "h" => 4, "i" => 1, "j" => 8, "k" => 5, "l" => 1, "m" => 3, "n" => 1, "o" => 1, "p" => 3, "q" => 10, "r" => 1, "s" => 1, "t" => 1, "u" => 1, "v" => 4, "w" => 4, "x" => 8, "y" => 4, "z" => 10 }
+  def initialize ()
   end
 
-
-  def scrabble_score()
+  def scrabble_score(word)
+    scores = { "a" => 1, "b" => 3, "c" => 3, "d" => 2, "e" => 1, "f" => 4, "g" => 2, "h" => 4, "i" => 1, "j" => 8, "k" => 5, "l" => 1, "m" => 3, "n" => 1, "o" => 1, "p" => 3, "q" => 10, "r" => 1, "s" => 1, "t" => 1, "u" => 1, "v" => 4, "w" => 4, "x" => 8, "y" => 4, "z" => 10 }
     total_score = 0
-    @word.each_char { | letter | total_score += @scores.fetch(letter) }
+    word.downcase().each_char { | letter | total_score += scores.fetch(letter) }
     return total_score
+  end
+end
+
+class Sentence < Word
+  def initialize()
+  end
+
+  def split(sentence)
+    array = sentence.split(" ")
+    return array
+  end
+
+  def add_sentence(words)
+    score = 0
+    (split(words)).each do |word|
+      score += scrabble_score(word)
+    end
+    return score
   end
 end
--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -3,12 +3,13 @@ require('scrabble_score')
 
 describe('Word#scrabble_score') do
   it("returns a scrabble score for a letter") do
-    word = Word.new("a")
-    expect(word.scrabble_score()).to(eq(1))
+    word = Word.new()
+    expect(word.scrabble_score('Hello')).to(eq(8))
   end
-
+end
+describe('Sentence#scrabble_score') do
   it("returns a scrabble score for the word 'hello' ") do
-    new_word = Word.new("Hello")
-    expect(new_word.scrabble_score()).to(eq(8))
+    new_sentence = Sentence.new()
+    expect(new_sentence.add_sentence("hello Hello")).to(eq(16))
   end
 end
